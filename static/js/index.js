01bf0c16d2bfe0318fb6b4b1cb12a8a9e3b5be38
haseeb1604 <mhaseeb1604@gmail.com>
2022-06-23 11:13:01 +0500

    javascript work done, Style & JavaScripts are minified and Bootstrap added

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,33 @@
-function main() {
+let popAlert = (alert, message, classAdd) =>{
+    alert.style.display = 'block';
+    alert.setAttribute("class", `alert alert-dismissible fade show ${classAdd}`);
+    alert.querySelector(".message").innerHTML = message;
+    alert.querySelector("button").addEventListener("click", function(e) {
+        this.parentElement.style.display= "none";
+    })
+}
+
+let progressBar = (val, progress) =>{
+    if(val){
+        progress.style.display = "block";
+        const bar = progress.querySelector(".progress-bar");
+        bar.style.width = `${val}%`;
+        bar.setAttribute("aria-valuenow", val);
+        bar.innerHTML = `${val}%`;
+        return;
+    }
+    progress.style.display = "none";
+}
+
+window.addEventListener('DOMContentLoaded',function(){
     const image_input = document.querySelector("#img_input");
     const form = document.querySelector("#myform");
     const pred = document.querySelector('.pred');
+    const alert = document.querySelector('#alert');
+    const progress = document.querySelector('#progress');
     let uploaded_img = "";
 
+    // Display Image 
     image_input.addEventListener("change", function(){
          const reader = new FileReader();
          reader.addEventListener("load", () => {
@@ -13,6 +37,7 @@ function main() {
          reader.readAsDataURL(this.files[0])
     })
 
+    // Form Submit
     form.addEventListener("submit", e => {
         e.preventDefault();
         const image = image_input.files[0]
@@ -24,23 +49,18 @@ function main() {
                     url: "https://gemsapi-d7s57.ondigitalocean.app/image",
                     method: "POST",
                     data: formdata,
-                    onsuccess: (responseText, status, statusText)=>{
+                    onsuccess: (responseText)=>{
                         var data = JSON.parse(responseText);
                         pred.innerHTML = data['pred'];
+                        popAlert(alert, `Image has been successfully Predicted`, "alert-success");
                     },
-                    onfailure: (status, statusText)=>{
-                        pred.innerHTML = `<p>Status : ${status}</p> <p>${statusText}</p>`
-                    },
+                    onprogress: (val) =>progressBar(val, progress),
+                    onfailure: (status, statusText)=>popAlert(alert, `${status} - ${statusText}`, "alert-danger"),
                     contentType: false,
                 });
-            }else{
-                pred.innerHTML = "Image size must be less than 4 mb";
-            }
-        }else{
-            pred.innerHTML = "Image type not supported";
-        }
+            }else
+                popAlert(alert, "Image size must be upto 4 mb only", "alert-warning");
+        }else
+            popAlert(alert, "Image type not supported. Please select a supported image type(png, jpeg, jpg)", "alert-warning");
     })
-
-}
-
-window.addEventListener('DOMContentLoaded', main);
+})
